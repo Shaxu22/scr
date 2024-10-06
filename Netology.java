6e708d5061de185fa6f79f3b3e1f9279d38f6523
shaxu22 <shaxu@rambler.ru>
2024-10-06 20:23:53 +0300

    first commit

---
+++ Netology.java
@@ -0,0 +1,28 @@
+import javax.swing.text.DefaultEditorKit;
+
+public class Netology {
+
+    public static void main(String[] args) {
+
+        int price = 1532;
+        int bonus= 20;
+        int bonus_miles = price / bonus;
+        System.out.println("Ваш бонус составляет " + bonus_miles + " миль");
+
+
+//        int percent;
+//        if (isRegistered) {
+//            percent = 3;
+//        } else {
+//            percent = 1;
+//        }
+//
+////        int percent = isRegistered ? 3 : 1;
+//        int bonus = amount * percent / 100;
+//        if (bonus > 500) {
+//            bonus = 500;
+//        }
+//        System.out.println(" Итоговый бонус: " + bonus);
+    }
+
+}
